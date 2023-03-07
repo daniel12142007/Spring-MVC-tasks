a25b8412b257d1f4ee9dd7ebdabf268117825dc0
daniel12142007 <danijelahatzanov@gmail.com>
2023-03-07 18:01:40 +0600

    PostMapping, GetMapping,

--- src/main/java/spring/springmvcconfig/Controler.java
+++ src/main/java/spring/springmvcconfig/Controler.java
@@ -1,11 +1,76 @@
 package spring.springmvcconfig;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import spring.model.*;
+import spring.model.repository.interfaces.Repositorys;
 
 @Controller
 public class Controler {
+    private final Repositorys<Company, Course, Groups, Teacher, Student> repositorys;
 
+    public Controler(Repositorys<Company, Course, Groups, Teacher, Student> repositorys) {
+        this.repositorys = repositorys;
+    }
 
+    @GetMapping("/")
+    public String findAll(Model model) {
+        model.addAttribute("all", repositorys.findAllCompany());
+        return "find-allCompany";
+    }
+
+    @PostMapping("/saveCompany")
+    private String saveCompany(@RequestParam("companyName") String companyName, @RequestParam("locatedCountry") String locatedCountry) {
+        Company company = new Company();
+        company.setCompanyName(companyName);
+        company.setLocatedCountry(locatedCountry);
+        return "redirect:/";
+    }
+
+    @GetMapping("/companyForm")
+    public String saveFormCompany() {
+        return "formCompany";
+    }
+
+    @GetMapping("/deletebyCompany/{id}")
+    public String deletebyidCompany(@PathVariable int id) {
+        Company company = repositorys.findByIdCompany(id);
+        repositorys.deleteCompany(company.getId());
+        return "redirect:/";
+    }
+
+    @GetMapping("/getCompany/by/{id}")
+    public String getbyIdCompany(Model model, @PathVariable int id) {
+        Company company = repositorys.findByIdCompany(id);
+        model.addAttribute("company", company);
+        return "findCompany";
+    }
+
+    @GetMapping("/updateCompany/{id}")
+    public String updatePersonForm(@PathVariable("id") int id, Model model) {
+        Company company = repositorys.findByIdCompany(id);
+        model.addAttribute("person", company);
+        return "update-company-form";
+    }
+
+    @PostMapping("/real/update/{id}")
+    public String updatePerson(@RequestParam("name") String name, @RequestParam("countr") String country, @PathVariable int id) {
+        Company company = new Company();
+        company.setCompanyName(name);
+        company.setLocatedCountry(country);
+        repositorys.updateCompany(id, company);
+        return "redirect:/";
+    }
+
+    @GetMapping("/clearCompany")
+    public String clear() {
+        repositorys.clearCompany();
+        return "redirect:/";
+    }
 }
 // private final PersonDao personDao;
 //
