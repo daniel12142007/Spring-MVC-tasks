fac5ca85c39a978b4caeab1199077034cf6d23f1
daniel12142007 <danijelahatzanov@gmail.com>
2023-03-08 20:25:16 +0600

    PostMapping, GetMapping,

--- src/main/java/spring/url/App.java
+++ src/main/java/spring/url/App.java
@@ -1,35 +1,29 @@
 package spring.url;
 
-import spring.model.*;
-import spring.model.enums.Studentforms;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class App {
     public static void main(String[] args) {
-        Repository repository1 = new Repository();
-        List<Course> courses = new ArrayList<>();
-        List<Groups> groups = new ArrayList<>();
-        List<Student> students = new ArrayList<>();
-        Company company = new Company("Iphone", "USA");
-        Course course = new Course("java", "9");
-        Teacher teacher = new Teacher("Azamat", "Azamatovich", "azamat@gmail.com");
-        teacher.setCourse(course);
-        course.setTeacher(teacher);
-        course.setCompany(company);
-        courses.add(course);
-        company.setCourses(courses);
-        Groups group = new Groups("java A ultimate", "2022", "2023");
-        group.setCourses(courses);
-        Student student = new Student("Daniel", "Ahatzanov", "daniel@gmail,com", Studentforms.OFFLINE);
-        student.setGroup(group);
-        students.add(student);
-        group.setStudents(students);
-        group.setCompany(company);
-        groups.add(group);
-        course.setGroups(groups);
-        company.setGroups(groups);
-        repository1.save(company);
+//        Repository repository1 = new Repository();
+//        List<Course> courses = new ArrayList<>();
+//        List<Groups> groups = new ArrayList<>();
+//        List<Student> students = new ArrayList<>();
+//        Company company = new Company("Iphone", "USA");
+//        Course course = new Course("java", "9");
+//        Teacher teacher = new Teacher("Azamat", "Azamatovich", "azamat@gmail.com");
+//        teacher.setCourse(course);
+//        course.setTeacher(teacher);
+//        course.setCompany(company);
+//        courses.add(course);
+//        company.setCourses(courses);
+//        Groups group = new Groups("java A ultimate", "2022", "2023");
+//        group.setCourses(courses);
+//        Student student = new Student("Daniel", "Ahatzanov", "daniel@gmail,com", Studentforms.OFFLINE);
+//        student.setGroup(group);
+//        students.add(student);
+//        group.setStudents(students);
+//        group.setCompany(company);
+//        groups.add(group);
+//        course.setGroups(groups);
+//        company.setGroups(groups);
+//        repository1.save(company);
     }
 }
