1f060f47289587b8fd69019536a74f08045e3ebb
daniel12142007 <danijelahatzanov@gmail.com>
2023-03-11 23:50:09 +0600

    deleted App,Repository,Util

--- src/main/java/spring/Stringconfig/Myconfig.java
+++ src/main/java/spring/Stringconfig/Myconfig.java
@@ -1,5 +1,6 @@
 package spring.Stringconfig;
 
+import org.hibernate.cfg.Environment;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 @ComponentScan("spring")
@@ -34,9 +36,17 @@ public class Myconfig implements WebMvcConfigurer {
         LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
         sessionFactoryBean.setDataSource(dataSource());
         sessionFactoryBean.setPackagesToScan(new String[]{"spring.model"});
+        sessionFactoryBean.setHibernateProperties(hibernateProperties());
         return sessionFactoryBean;
     }
 
+    private Properties hibernateProperties() {
+        Properties properties = new Properties();
+        properties.setProperty(Environment.DIALECT, "org.hibernate.dialect.PostgreSQLDialect");
+        properties.setProperty(Environment.SHOW_SQL, "true");
+        properties.setProperty(Environment.HBM2DDL_AUTO, "update");
+        return properties;
+    }
 
     @Bean
     public HibernateTransactionManager transactionManager() {
--- src/main/webapp/WEB-INF/view/save.jsp
+++ src/main/webapp/WEB-INF/view/save.jsp
@@ -14,8 +14,10 @@
 <%--@RequestParam("companyName") String companyName, @RequestParam("locatedCountry") String locatedCountry--%>
 <div class="bg">
     <form action="/saveCompany" method="post">
-        <label>Company Name</label><br><input type="text" name="companyName"><br>
-        <label>LocatedCountry</label><br><input type="text" name="locatedCountry"><br>
+        <label>Company Name</label><br>
+        <input type="text" name="companyName"><br>
+        <label>LocatedCountry</label><br>
+        <input type="text" name="locatedCountry"><br>
         <input type="submit" value="register company">
     </form>
 </div>
